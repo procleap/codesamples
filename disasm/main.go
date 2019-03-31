44325ab7ea6953b4fdd19e7d71f60ef681b0cd00
procleap <procleap@users.noreply.github.com>
2019-03-31 17:03:23 +0200

    Implement Disasm()

--- disasm/main.go
+++ disasm/main.go
@@ -5,6 +5,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"golang.org/x/arch/x86/x86asm"
+)
+
+const (
+	bit32 = 32
+	bit64 = 64
 )
 
 func main() {
@@ -15,5 +22,16 @@ func main() {
 	defer f.Close()
 
 	code, _ := f.Section(".text").Data()
-	fmt.Printf("% x\n", code)
+	Disasm(code, bit32)
+}
+
+// Disasm disassembles either 32 or 64-bit intel instructions.
+func Disasm(data []byte, bit uint64) {
+	p := 0
+	for p < len(data) {
+		op, _ := x86asm.Decode(data[p:], int(bit))
+		x86asm.IntelSyntax(op, bit, nil)
+		fmt.Println(x86asm.IntelSyntax(op, bit, nil))
+		p += op.Len
+	}
 }
