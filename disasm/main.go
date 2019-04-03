58578baa2bbe80cf6df036f96970bbc5bcb14294
procleap <procleap@users.noreply.github.com>
2019-04-03 11:53:10 +0200

    Move disassembly logic to disasm.go

--- disasm/main.go
+++ disasm/main.go
@@ -1,67 +1,8 @@
 package main
 
-import (
-	"debug/pe"
-	"fmt"
-	"log"
-	"os"
-	"sort"
-
-	"golang.org/x/arch/x86/x86asm"
-)
-
-// Processor modes: either 32 or 64-bit.
-const (
-	mode32 = 32
-	mode64 = 64
-)
+import "os"
 
 func main() {
-	f, err := pe.Open(os.Args[1])
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer f.Close()
-
-	// Get PE EntryPoint.
-	var ib, entryPoint, mode uint64
-	switch oh := f.OptionalHeader.(type) {
-	case *pe.OptionalHeader32:
-		ib = uint64(oh.ImageBase)
-		entryPoint = ib + uint64(oh.AddressOfEntryPoint)
-		mode = mode32
-	case *pe.OptionalHeader64:
-		ib = oh.ImageBase
-		entryPoint = ib + uint64(oh.AddressOfEntryPoint)
-		mode = mode64
-	}
-
-	code, _ := f.Section(".text").Data()
-	Disasm(code, mode, entryPoint)
-}
-
-// Disasm disassembles either 32 or 64-bit intel instructions.
-func Disasm(data []byte, mode uint64, base uint64) {
-	disasList := make(map[int]string)
-	p := 0
-
-	for p < len(data) {
-		op, _ := x86asm.Decode(data[p:], int(mode))
-		x86asm.IntelSyntax(op, mode, nil)
-		disasList[p] = x86asm.IntelSyntax(op, mode, nil)
-		p += op.Len
-	}
-
-	Print(disasList, base)
-}
-
-func Print(m map[int]string, base uint64) {
-	var keys []int
-	for k := range m {
-		keys = append(keys, k)
-	}
-	sort.Ints(keys)
-	for _, k := range keys {
-		fmt.Printf("%x:    %s\n", uint64(k)+base, m[k])
-	}
+	d := NewDisasm(os.Args[1])
+	d.Disasm()
 }
