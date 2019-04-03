119faf55068c09a3d105c122f5f80a6e37844184
procleap <procleap@users.noreply.github.com>
2019-04-03 12:27:35 +0200

    Implement Usage()

--- disasm/main.go
+++ disasm/main.go
@@ -1,8 +1,18 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
 func main() {
 	d := NewDisasm(os.Args[1])
 	d.Disasm()
+}
+
+// Usage prints the program usage to stdout and exit.
+func Usage() {
+	fmt.Printf("Usage:\n\t%s <file.exe>\n", filepath.Base(os.Args[0]))
+	os.Exit(1)
 }
